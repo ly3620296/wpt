8e92f6d4adc541c28587d5070fc03a1f21b3d37a
ly3620296 <18844092305@139.com>
2019-05-07 19:22:20 +0800

    gggg

--- wxyfwpt/web/js-lib/wpt/foot.js
+++ wxyfwpt/web/js-lib/wpt/foot.js
@@ -8,16 +8,13 @@ $("#wpt_foot>li").on("click", function () {
 
 $(document).ready(function () {
     var url = window.location.href;
-    if (url.search("module/main/main.jsp") != -1) {
-        $(".footer-menu li").eq(0).find("i").addClass("active");
-    }
     if (url.search("module/fwzx/fwzxapp.jsp") != -1) {
         $(".footer-menu li").eq(1).find("i").addClass("active");
-    }
-    if (url.search("module/msg/msg.jsp") != -1) {
+    } else if (url.search("module/msg/msg.jsp") != -1) {
         $(".footer-menu li").eq(2).find("i").addClass("active");
-    }
-    if (url.search("module/my/my.jsp") != -1) {
+    } else if (url.search("module/my/my.jsp") != -1) {
         $(".fa-user").addClass("active");
+    } else {
+        $(".footer-menu li").eq(0).find("i").addClass("active");
     }
 })
