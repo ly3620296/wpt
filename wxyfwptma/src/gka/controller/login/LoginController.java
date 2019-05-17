8ecd93c8d067d9eff22b80c79c63c34403fb2f60
ly3620296 <18844092305@139.com>
2019-05-17 16:53:05 +0800

    111

--- wxyfwptma/src/gka/controller/login/LoginController.java
+++ wxyfwptma/src/gka/controller/login/LoginController.java
@@ -13,6 +13,7 @@ import gka.system.ReturnInfo;
 @ControllerBind(controllerKey = "/login")
 public class LoginController extends Controller {
     private LoginDao loginDao = new LoginDao();
+
     public void loginValidate() {
         ReturnInfo returnInfo = new ReturnInfo();
         try {
@@ -33,36 +34,5 @@ public class LoginController extends Controller {
         }
         renderJson(returnInfo);
 
-    }
-
-    private void setSession(Record record) {
-        WptMaUserInfo userInfo = new WptMaUserInfo();
-        userInfo.setZh(record.getStr("ZH"));
-        userInfo.setXm(record.getStr("XM"));
-        userInfo.setXb(record.getStr("XB"));
-        userInfo.setMz(record.getStr("MZ"));
-        userInfo.setZzmm(record.getStr("ZZMM"));
-        userInfo.setCsrq(record.getStr("CSRQ"));
-        userInfo.setZjlx(record.getStr("ZJLX"));
-        userInfo.setZjhm(record.getStr("ZJHM"));
-        userInfo.setJgdm(record.getStr("JGDM"));
-        userInfo.setJgmc(record.getStr("JGMC"));
-        userInfo.setZydm(record.getStr("ZYDM"));
-        userInfo.setZymc(record.getStr("ZYMC"));
-        userInfo.setBjdm(record.getStr("BJDM"));
-        userInfo.setBjmc(record.getStr("BJMC"));
-        userInfo.setNjdm(record.getStr("NJDM"));
-        userInfo.setNjmc(record.getStr("NJMC"));
-        userInfo.setXz(record.getStr("XZ"));
-        userInfo.setSfzx(record.getStr("SFZX"));
-        userInfo.setXjzt(record.getStr("XJZT"));
-        userInfo.setBdzc(record.getStr("BDZC"));
-        userInfo.setLxdh(record.getStr("LXDH"));
-        userInfo.setYx(record.getStr(" YX"));
-        userInfo.setJsdm(record.getStr("JSDM"));
-        userInfo.setJsmc(record.getStr("JSMC"));
-        userInfo.setZyfxdm(record.getStr("ZYFXDM"));
-        userInfo.setZyfxmc(record.getStr("ZYFXMC"));
-        getSession().setAttribute("wptUserInfo", userInfo);
     }
 }
