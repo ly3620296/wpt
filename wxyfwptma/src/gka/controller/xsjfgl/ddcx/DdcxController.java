ea8e193e6d62f1f3517a6e844e2885f2d71de18c
ly3620296 <18844092305@139.com>
2019-11-09 22:00:47 +0800

    gggga

--- wxyfwptma/src/gka/controller/xsjfgl/ddcx/DdcxController.java
+++ wxyfwptma/src/gka/controller/xsjfgl/ddcx/DdcxController.java
@@ -1,92 +1,92 @@
-package gka.controller.xsjfgl.ddcx;
-
-
-import com.alibaba.druid.util.StringUtils;
-import com.jfinal.core.Controller;
-import com.jfinal.ext.route.ControllerBind;
-import com.jfinal.plugin.activerecord.Page;
-import com.jfinal.plugin.activerecord.Record;
-import gka.common.kit.ReKit;
-import gka.controller.xsjfgl.wyjf.JfInfo;
-import gka.controller.xsjfgl.wyjf.WyjfDao;
-import gka.pay.wxpay.controller.MyWxpayConstant;
-import gka.xsjfgl.login.WptMaXSUserInfo;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@ControllerBind(controllerKey = "/xsjfgl/ddcx")
-public class DdcxController extends Controller {
-    private DdcxDao ddcxDao = new DdcxDao();
-    private WyjfDao wyjfDao = new WyjfDao();
-
-    public void index() {
-        Map map = new HashMap();
-        try {
-            WptMaXSUserInfo userInfo = (WptMaXSUserInfo) getSession().getAttribute("wptMaXSUserInfo");
-            String xh = userInfo.getZh();
-            int page = Integer.parseInt(getPara("page"));
-            int limit = Integer.parseInt(getPara("limit"));
-            Page<Record> paginate = ddcxDao.getOrderInfo(page, limit, xh);
-            map.put("code", "0");
-            map.put("msg", "success");
-            map.put("data", paginate.getList());
-            map.put("count", paginate.getTotalRow());
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("code", "-1");
-            map.put("msg", "系统繁忙，请稍后重试！");
-        }
-        renderJson(map);
-    }
-
-    public void qxInfo() {
-        Map map = new HashMap();
-        try {
-            String xn = getPara("xn");
-            String order_no = getPara("order_no");
-            if (!StringUtils.isEmpty(xn) && !StringUtils.isEmpty(order_no)) {
-                WptMaXSUserInfo userInfo = (WptMaXSUserInfo) getSession().getAttribute("wptMaXSUserInfo");
-                String xh = userInfo.getZh();
-                List<Record> titles = wyjfDao.queryTitle();
-                Record re = wyjfDao.jf(xh, xn, titles);
-                List<JfInfo> jfInfoList = getJfinfo(titles, re);
-                map.put("data", jfInfoList);
-                map.put("code", "0");
-                map.put("msg", "success");
-            } else {
-                map.put("code", "-2");
-                map.put("msg", "请从正规渠道支付！");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("code", "-1");
-            map.put("msg", "系统繁忙，请稍后重试！");
-        }
-        renderJson(map);
-    }
-
-    private List<JfInfo> getJfinfo(List<Record> titles, Record re) {
-        List<JfInfo> jfInfos = new ArrayList<JfInfo>();
-        for (Record t : titles) {
-            JfInfo jfInfo = new JfInfo();
-            String sfjf = t.getStr("SFBX");
-            if (sfjf.equals("1")) {
-                jfInfo.setXmmc(t.getStr("JFXMMC"));
-                jfInfo.setXmid(t.getStr("JFXMID"));
-                jfInfo.setSfbx(sfjf);
-                jfInfo.setJfje(re.getStr(jfInfo.getXmid()));
-            } else {
-                jfInfo.setXmmc(t.getStr("JFXMMC") + "（选交）");
-                jfInfo.setXmid(t.getStr("JFXMID"));
-                jfInfo.setSfbx(sfjf);
-                jfInfo.setJfje(re.getStr(jfInfo.getXmid()));
-            }
-            jfInfos.add(jfInfo);
-        }
-        return jfInfos;
-    }
-}
+//package gka.controller.xsjfgl.ddcx;
+//
+//
+//import com.alibaba.druid.util.StringUtils;
+//import com.jfinal.core.Controller;
+//import com.jfinal.ext.route.ControllerBind;
+//import com.jfinal.plugin.activerecord.Page;
+//import com.jfinal.plugin.activerecord.Record;
+//import gka.common.kit.ReKit;
+//import gka.controller.xsjfgl.wyjf.JfInfo;
+//import gka.controller.xsjfgl.wyjf.WyjfDao;
+//import gka.pay.wxpay.controller.MyWxpayConstant;
+//import gka.xsjfgl.login.WptMaXSUserInfo;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//@ControllerBind(controllerKey = "/xsjfgl/ddcx")
+//public class DdcxController extends Controller {
+//    private DdcxDao ddcxDao = new DdcxDao();
+//    private WyjfDao wyjfDao = new WyjfDao();
+//
+//    public void index() {
+//        Map map = new HashMap();
+//        try {
+//            WptMaXSUserInfo userInfo = (WptMaXSUserInfo) getSession().getAttribute("wptMaXSUserInfo");
+//            String xh = userInfo.getZh();
+//            int page = Integer.parseInt(getPara("page"));
+//            int limit = Integer.parseInt(getPara("limit"));
+//            Page<Record> paginate = ddcxDao.getOrderInfo(page, limit, xh);
+//            map.put("code", "0");
+//            map.put("msg", "success");
+//            map.put("data", paginate.getList());
+//            map.put("count", paginate.getTotalRow());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            map.put("code", "-1");
+//            map.put("msg", "系统繁忙，请稍后重试！");
+//        }
+//        renderJson(map);
+//    }
+//
+//    public void qxInfo() {
+//        Map map = new HashMap();
+//        try {
+//            String xn = getPara("xn");
+//            String order_no = getPara("order_no");
+//            if (!StringUtils.isEmpty(xn) && !StringUtils.isEmpty(order_no)) {
+//                WptMaXSUserInfo userInfo = (WptMaXSUserInfo) getSession().getAttribute("wptMaXSUserInfo");
+//                String xh = userInfo.getZh();
+//                List<Record> titles = wyjfDao.queryTitle();
+//                Record re = wyjfDao.jf(xh, xn, titles);
+//                List<JfInfo> jfInfoList = getJfinfo(titles, re);
+//                map.put("data", jfInfoList);
+//                map.put("code", "0");
+//                map.put("msg", "success");
+//            } else {
+//                map.put("code", "-2");
+//                map.put("msg", "请从正规渠道支付！");
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            map.put("code", "-1");
+//            map.put("msg", "系统繁忙，请稍后重试！");
+//        }
+//        renderJson(map);
+//    }
+//
+//    private List<JfInfo> getJfinfo(List<Record> titles, Record re) {
+//        List<JfInfo> jfInfos = new ArrayList<JfInfo>();
+//        for (Record t : titles) {
+//            JfInfo jfInfo = new JfInfo();
+//            String sfjf = t.getStr("SFBX");
+//            if (sfjf.equals("1")) {
+//                jfInfo.setXmmc(t.getStr("JFXMMC"));
+//                jfInfo.setXmid(t.getStr("JFXMID"));
+//                jfInfo.setSfbx(sfjf);
+//                jfInfo.setJfje(re.getStr(jfInfo.getXmid()));
+//            } else {
+//                jfInfo.setXmmc(t.getStr("JFXMMC") + "（选交）");
+//                jfInfo.setXmid(t.getStr("JFXMID"));
+//                jfInfo.setSfbx(sfjf);
+//                jfInfo.setJfje(re.getStr(jfInfo.getXmid()));
+//            }
+//            jfInfos.add(jfInfo);
+//        }
+//        return jfInfos;
+//    }
+//}
