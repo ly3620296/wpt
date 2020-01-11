856f2506c26af60da19683c8683aa2866e5973f3
ly3620296 <18844092305@139.com>
2020-01-11 19:37:32 +0800

    gggga

---
+++ wxysfma/src/gka/lsjfgl/login/WptMaLSUserInfo.java
@@ -0,0 +1,250 @@
+package gka.lsjfgl.login;
+
+/**
+ * 用户基本信息
+ */
+public class WptMaLSUserInfo {
+    private String zh;    //账号 学号或工号
+    private String xm;    //姓名
+    private String xb;    //性别
+    private String mz;    //民族
+    private String zzmm;  //政治面貌
+    private String csrq;  //出生日期
+    private String zjlx;  //证件类型
+    private String zjhm;  //证件号码
+    private String jgdm;  //机构代码
+    private String jgmc;  //机构名称
+    private String zydm;  //专业代码
+    private String zymc;  //专业名称
+    private String bjdm;  //班级代码
+    private String bjmc;  //班级名称
+    private String njdm;  //年级代码
+    private String njmc;  //年级名称
+    private String xz;    //学制
+    private String sfzx;  //是否在校
+    private String xjzt;  //学籍状态
+    private String bdzc;  //报道注册标记
+    private String lxdh;  //联系电话
+    private String yx;    //邮箱
+    private String jsdm;  //角色代码
+    private String jsmc;  //角色名称
+    private String zyfxdm; //专业方向代码
+    private String zyfxmc; //专业方向名称
+    private String openId;
+
+    public String getZh() {
+        return zh;
+    }
+
+    public void setZh(String zh) {
+        this.zh = zh;
+    }
+
+    public String getXm() {
+        return xm;
+    }
+
+    public void setXm(String xm) {
+        this.xm = xm;
+    }
+
+    public String getXb() {
+        return xb;
+    }
+
+    public void setXb(String xb) {
+        this.xb = xb;
+    }
+
+    public String getMz() {
+        return mz;
+    }
+
+    public void setMz(String mz) {
+        this.mz = mz;
+    }
+
+    public String getZzmm() {
+        return zzmm;
+    }
+
+    public void setZzmm(String zzmm) {
+        this.zzmm = zzmm;
+    }
+
+    public String getCsrq() {
+        return csrq;
+    }
+
+    public void setCsrq(String csrq) {
+        this.csrq = csrq;
+    }
+
+    public String getZjlx() {
+        return zjlx;
+    }
+
+    public void setZjlx(String zjlx) {
+        this.zjlx = zjlx;
+    }
+
+    public String getZjhm() {
+        return zjhm;
+    }
+
+    public void setZjhm(String zjhm) {
+        this.zjhm = zjhm;
+    }
+
+    public String getJgdm() {
+        return jgdm;
+    }
+
+    public void setJgdm(String jgdm) {
+        this.jgdm = jgdm;
+    }
+
+    public String getJgmc() {
+        return jgmc;
+    }
+
+    public void setJgmc(String jgmc) {
+        this.jgmc = jgmc;
+    }
+
+    public String getZydm() {
+        return zydm;
+    }
+
+    public void setZydm(String zydm) {
+        this.zydm = zydm;
+    }
+
+    public String getZymc() {
+        return zymc;
+    }
+
+    public void setZymc(String zymc) {
+        this.zymc = zymc;
+    }
+
+    public String getBjdm() {
+        return bjdm;
+    }
+
+    public void setBjdm(String bjdm) {
+        this.bjdm = bjdm;
+    }
+
+    public String getBjmc() {
+        return bjmc;
+    }
+
+    public void setBjmc(String bjmc) {
+        this.bjmc = bjmc;
+    }
+
+    public String getNjdm() {
+        return njdm;
+    }
+
+    public void setNjdm(String njdm) {
+        this.njdm = njdm;
+    }
+
+    public String getNjmc() {
+        return njmc;
+    }
+
+    public void setNjmc(String njmc) {
+        this.njmc = njmc;
+    }
+
+    public String getXz() {
+        return xz;
+    }
+
+    public void setXz(String xz) {
+        this.xz = xz;
+    }
+
+    public String getSfzx() {
+        return sfzx;
+    }
+
+    public void setSfzx(String sfzx) {
+        this.sfzx = sfzx;
+    }
+
+    public String getXjzt() {
+        return xjzt;
+    }
+
+    public void setXjzt(String xjzt) {
+        this.xjzt = xjzt;
+    }
+
+    public String getBdzc() {
+        return bdzc;
+    }
+
+    public void setBdzc(String bdzc) {
+        this.bdzc = bdzc;
+    }
+
+    public String getLxdh() {
+        return lxdh;
+    }
+
+    public void setLxdh(String lxdh) {
+        this.lxdh = lxdh;
+    }
+
+    public String getYx() {
+        return yx;
+    }
+
+    public void setYx(String yx) {
+        this.yx = yx;
+    }
+
+    public String getJsdm() {
+        return jsdm;
+    }
+
+    public void setJsdm(String jsdm) {
+        this.jsdm = jsdm;
+    }
+
+    public String getJsmc() {
+        return jsmc;
+    }
+
+    public void setJsmc(String jsmc) {
+        this.jsmc = jsmc;
+    }
+
+    public String getZyfxdm() {
+        return zyfxdm;
+    }
+
+    public void setZyfxdm(String zyfxdm) {
+        this.zyfxdm = zyfxdm;
+    }
+
+    public String getZyfxmc() {
+        return zyfxmc;
+    }
+
+    public void setZyfxmc(String zyfxmc) {
+        this.zyfxmc = zyfxmc;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+}
